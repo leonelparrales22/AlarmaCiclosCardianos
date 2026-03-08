36e82870287ab4d77ea961e784169c510c751cfc
Leonel Parrales <leonelparrales22@gmail.com>
2026-03-08 17:43:09 -0500

    Navegación a ConfigActivity desde MainActivity

--- app/src/main/java/com/turingtech/circadianos/MainActivity.java
+++ app/src/main/java/com/turingtech/circadianos/MainActivity.java
@@ -1,6 +1,8 @@
 package com.turingtech.circadianos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.turingtech.circadianos.ui.config.ConfigActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_view);
+
+        MaterialButton configureWindowButton = findViewById(R.id.configureWindowButton);
+        configureWindowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
